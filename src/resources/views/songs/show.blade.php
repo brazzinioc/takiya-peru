b00fe45c99fcf601cf4729ce52c2665a0f63863e
Brazzini OC <brazziniogosi@gmail.com>
2021-12-01 12:08:58 -0500

    add css class

--- src/resources/views/songs/show.blade.php
+++ src/resources/views/songs/show.blade.php
@@ -57,11 +57,11 @@
 
     <div class="song-image-cover flex px-4 lg:px-8 py-10" style="background-image: url('{{$urlImage}}')">
         <div class="bg-white text-black p-5 place-self-end w-full lg:w-auto border shadow">
-            <h1 class="font-medium text-purple-700 mb-1">{{strtoupper($song->title)}}</h1>
+            <h1 class="font-medium text-purple-700 text-2xl mb-1">{{strtoupper($song->title)}}</h1>
             <p class="text-xs lg:text-base">Género musical: <span class="font-semibold"> {{ $song->genre->name }}  </p>
             <p class="text-xs lg:text-base">Autor: <span class="font-semibold"> {{ $song->author->name_lastname }}</p>
             <p class="text-xs lg:text-base">Letras: <span class="font-semibold"> {{ $song->writer->name }} </span> </p>
-            <p class="text-xs lg:text-base font-light">Actualización: <span class=""> {{ $song->updated_at->diffForHumans() }} </span> </p>
+            <p class="text-xs lg:text-base font-light"> <small> Actualización: <span class=""> {{ $song->updated_at->diffForHumans() }} </span> </small></p>
         </div>
     </div>
 
